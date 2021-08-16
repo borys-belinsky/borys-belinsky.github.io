c7dee7d063c0823980ceb3dedf8eff37bcc2cf81
Borys Bielinskyi <borys.bielinskyi@airship.com>
2021-08-16 15:10:48 +0300

    Addd web SDK

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1629115418.1918838
+importScripts('https://aswpsdkeu.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://borys\u002Dbelinsky.github.io/logo.png',
+  defaultTitle: 'Look Ma, I am a title',
+  defaultActionURL: 'https://borys\u002Dbelinsky.github.io/',
+  appKey: 'kV1VlqzkQ0OZmSaLvkPDmA',
+  token: 'MTprVjFWbHF6a1EwT1ptU2FMdmtQRG1BOnNoMmFONTZTTmdLQ3loN21DRk8xZWcyY2hISm5WajN5Z0wzc3FISjBuOXM',
+  vapidPublicKey: 'BDTQvbPfcH3wJFnfFnizl1_0f2_24WJbX7VzQvlhk3bCczGtrfUw_ZzgPhFgXBCecYMFtWA15YphT0jq2Ho7VmE='
+})
